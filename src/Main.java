5dfa8b314386fc103f909e1fae0d68583a59d933
iago-hub <iagoalexandrensc@gmail.com>
2022-06-01 07:29:33 -0300

    Revisão Java - Processamento de Dados (Atirbuição / Casting)

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,75 @@
-import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-	int y = 32;
-	System.out.println(y);
-	
-	
-	double x = 10.35784;
+	//   Processamento de Dados
 		
-	System.out.println(x);
+	System.out.println("-------------------Exemplo 1--------------------");
+		int x, y;
+		
+		x = 5;
+		
+		y= 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println("--------------------Exemplo 2--------------------");
+		
+		int w;
+		
+		double z;
+		
+		w = 5;
+		z = 2 * w;
+		
+		System.out.println(w);
+		System.out.println(z);
+		
+		System.out.println("---------------------Exemplo 3----------------");
+		
+		double b, B, h, area;
+		
+		b=6.0;
+		B=8.0;
+		h=5.0;
+		
+		area = (b+B)/ 2.0 * h;
+		
+		System.out.println(area);
+		
+		
+		System.out.println("-------------------Exemplo 4------------------");
+		
+		int c, d;
+		double resultado;
+		
+		c = 5;
+		d = 2;
+		
+		resultado = c / d;
+		
+		System.out.println(resultado);
+		
+		//Avisar o compilador que quero receber o resultado da divisão um double
+		System.out.println("-- Casting = fundição --"); 
+		
+		resultado = (double) c / d;
+		
+		System.out.println(resultado);
+		
+		System.out.println("------------------Exemplo 5 (Casting)-------------------");
+		
+		double e;
+		int f;
+		
+		e = 5.0;
+		f = (int) e;
+		
+		System.out.println(f);
+		
 	
-	/* Controlar a quantidade de casas decimais. */
-	
-	System.out.printf("%.10f%n", x);
-	System.out.printf("%.1f\n",x);
-	System.out.printf("%.4f%n", x);
-	
-	
-	/* Considerar o separador de decimais como PONTO, antes da declaração do Scanner*/
-	
-	Locale.setDefault(Locale.US);
-	System.out.printf("%.4f%n", x);
-	
-	
-	/* Concatenar vários elementos em um mesmo comando de escrita */
-	
-	System.out.println("Resultado = " + x + " Metros");
-	
-	/* Concatenar vários elementros em uma mesmo comando de escrita usando printf*/
-	
-	System.out.printf("Resultado = %.2f metro%n", x);
-	
-	/* Concatenar vários elementos em um mesmo coamndo de escrita*/
-	String nome = "Maria";
-	int idade = 31;
-	double renda = 4000.0;
-	System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome,idade,renda);
 		
 	}
 

43bb778739d4c1a9e9139995b946e317a1e478b0
iago-hub <iagoalexandrensc@gmail.com>
2022-06-11 23:59:06 -0300

    Revisão Java - Funções Matemáticas em Java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 public class Main {
 
@@ -8,48 +8,61 @@ public class Main {
 
 		System.out.println("-------------------Exemplo 1--------------------");
 		
-		// Para Ler um texto até a QUEBRA DE LINHA
-
-		Scanner sc = new Scanner(System.in);
-
-		String s1, s2, s3;
-
-		System.out.println("Digite 3 textos: ");
-
-		s1 = sc.nextLine();
-		s2 = sc.nextLine();
-		s3 = sc.nextLine();
-
-		System.out.println("Textos digitados: ");
-		System.out.println("Texto 1: " + s1);
-		System.out.println("Texto 2: " + s2);
-		System.out.println("Texto 3: " + s3);
-
+		// Funções Matemáticas do Java
 		
-
-		System.out.println("-----------------Exemplo 2----------------------");
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C, D ;
 		
-		// Atenção Exemplo de quebra de linha pendente
 		
-		int x;
-		String x1, x2, x3;
 		
-		System.out.println("Digite um valor inteiro e três textos : ");
+		A =  Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		D = A;
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25 é " + C);
 		
-		x = sc.nextInt();
-		sc.nextLine();
-		x1 = sc.nextLine();
-		x2 = sc.nextLine();
-		x3 = sc.nextLine();
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
 		
-		System.out.println("Dados digitados: ");
+		System.out.println(x + " Elevado a " + y + " = " + A);
+		System.out.println(x + " Elevado ao quadrado = " + B);
+		System.out.println("5 Elevado ao quadrado = " + C );
 		
-		System.out.println("Valor inteiro: " + x );
-		System.out.println("Texto 1: " + x1);
-		System.out.println("Texto 2: " + x2);
-		System.out.println("Texto 3: " + x3);
+		
+		A = Math.abs(y);
+		B = Math.abs(z);
+		C = Math.abs(D);
+		
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + z + " = " + B);
+		System.out.println("Valor absoluto de " + D + " = " + C);
 				
-		sc.close();
+		System.out.println("---------------------Exemplo 2-----------------------");
+		
+		// Equação do Segundo Grau
+		
+		double delta, a, b, c, x1, x2;
+		
+		a = 2.0;
+		b = -16.0;
+		c = -18.0;
+		
+		delta = (Math.pow(b, 2.0)) - (4*a*c);
+		
+		x1 = (-b + Math.sqrt(delta)) / (2.0*a);
+		x2 = (-b - Math.sqrt(delta)) / (2.0*a);
+		
+		System.out.println("Valor de Delta é: " + delta);
+		
+		System.out.printf("Portando, as raizes da equação: +(%.0fx^2) + (%.0fx) + (%.0f), são: x1 = %.2f e x2 = %.2f; ",a,b,c,x1,x2);  
+				
+		
+		
 	}
 
 }
